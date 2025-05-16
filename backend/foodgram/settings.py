9406d01d96e2c40be1804592bd7e152939becd16
Raul24556 <kirillmendrin245@yandex.com>
2025-05-16 18:48:44 +0300

    Исправлена ошибка 500: обновлена обработка коротких ссылок, добавлены протоколирование и разрешенные хосты

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -164,3 +164,21 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.db.backends": {
+            "level": "DEBUG",  # if DEBUG else 'ERROR',
+            "handlers": [
+                "console",
+            ],
+        },
+    },
+}
